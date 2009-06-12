332613d32994c8f91127255ab1eca1ea79a98cec
Larry Diehl <larry@laptoptheliquid.(none)>
2009-06-12 00:27:51 -0400

    Made endpoint less disruptive

--- lib/rack/client/resource.rb
+++ lib/rack/client/resource.rb
@@ -1,17 +1,21 @@
 class Rack::Client::Resource < Rack::Builder
   include Rack::Test::Methods
-
   def initialize(*args, &block)
-    @endpoint = Rack::Client::HTTP
+    @ran = false
+    super(*args, &block)
+  end
+
+  def run(*args, &block)
+    @ran = true
     super(*args, &block)
   end
 
   def endpoint(app, url = "http://example.org/")
-    @endpoint = Rack::URLMap.new(url => app)
+    run Rack::URLMap.new(url => app)
   end
 
   def app
-    run @endpoint
+    run Rack::Client::HTTP unless @ran
     to_app
   end
 end 
