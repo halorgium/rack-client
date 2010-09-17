251d51fdc648a7c1f0f5a5dfa79fe9304328af88
Ultra Ninja <ninja@engineyard.com>
2010-09-17 15:32:47 -0700

    Burn the specs.

--- spec/handler/excon_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe Rack::Client::Handler::Excon do
-  let(:client) { Rack::Client.new { run Rack::Client::Handler::Excon.new("http://localhost:#{server.port}") } }
-
-  it_should_behave_like "Synchronous Request API"
-end
--- spec/handler/net_http_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe Rack::Client::Handler::NetHTTP do
-  let(:client) { Rack::Client.new("http://localhost:#{server.port}") { run Rack::Client::Handler::NetHTTP } }
-
-  it_should_behave_like "Synchronous Request API"
-  it_should_behave_like "Asynchronous Request API"
-end
