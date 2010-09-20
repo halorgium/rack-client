1258dcb5be68d3f482588423417909ccdfad24c4
Matt Aimonetti <mattaimonetti@gmail.com>
2010-09-20 10:01:29 -0700

    made rack-client more Ruby 1.9.2 friendly

--- spec/auth/basic_spec.rb
+++ spec/auth/basic_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe Rack::Client::Auth::Basic do
   context 'Synchronous API' do
