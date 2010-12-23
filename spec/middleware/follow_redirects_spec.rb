6e83cea4c7fb1bde70dc1bc5f2eba4d7686c020a
Ben Burkert <ben@benburkert.com>
2010-12-23 10:54:30 -0800

    Excon issue fixed.

--- spec/middleware/follow_redirects_spec.rb
+++ spec/middleware/follow_redirects_spec.rb
@@ -4,10 +4,6 @@ describe Rack::Client::FollowRedirects do
   handler_contexts(Rack::Client::FollowRedirects) do
 
     it 'will follow a single redirect' do
-      if handler == Rack::Client::Handler::Excon
-        pending "https://github.com/geemus/excon/issues/issue/11"
-      end
-
       request   { get('/redirect/') }
       response  { body.should == 'Redirected' }
     end
