8435a0611bd1f3c947ebadf0c15640fc26010fdb
Ben Burkert <ben@benburkert.com>
2010-12-31 22:41:45 -0800

    EVEN MORE UNICODE FAIL.

--- spec/handler/em_http_spec.rb
+++ spec/handler/em_http_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
   describe Rack::Client::Handler::EmHttp do
     async_handler_context(Rack::Client::Handler::EmHttp) do
       it_should_behave_like "Handler API"
