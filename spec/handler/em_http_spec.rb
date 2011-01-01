da807716bf01b0473ee5faf74dde393e5d3c180d
Ben Burkert <ben@benburkert.com>
2010-12-31 22:30:16 -0800

    Ignore em-http & typhoeus tests for jruby.

--- spec/handler/em_http_spec.rb
+++ spec/handler/em_http_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
-describe Rack::Client::Handler::EmHttp do
-  async_handler_context(Rack::Client::Handler::EmHttp) do
-    it_should_behave_like "Handler API"
+unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+  describe Rack::Client::Handler::EmHttp do
+    async_handler_context(Rack::Client::Handler::EmHttp) do
+      it_should_behave_like "Handler API"
+    end
   end
 end
--- spec/handler/typhoeus_spec.rb
+++ spec/handler/typhoeus_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
-describe Rack::Client::Handler::Typhoeus do
-  async_handler_context(Rack::Client::Handler::Typhoeus) do
-    it_should_behave_like "Handler API"
-  end
+unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+  describe Rack::Client::Handler::Typhoeus do
+    async_handler_context(Rack::Client::Handler::Typhoeus) do
+      it_should_behave_like "Handler API"
+    end
 
-  sync_handler_context(Rack::Client::Handler::Typhoeus) do
-    it_should_behave_like "Handler API"
+    sync_handler_context(Rack::Client::Handler::Typhoeus) do
+      it_should_behave_like "Handler API"
+    end
   end
 end
